e2941c338e7338f58c41656ce61a83247995e38a
byunghyun <104744423+fridgecrew-byunghyun@users.noreply.github.com>
2023-11-27 12:27:18 +0900

    wip

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 /* NODEJS 필수 설정 */
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
+
 //const moment = require("moment");
 const helmet = require("helmet");
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const http = require("http").createServer(app);
+const mongoose = require("mongoose");
+const {boardRouter} = require('./routes/boardCRUD');
 const { Server } = require("socket.io");
 const io = new Server(http);
 
+
+
 /* NODEJS 필수 미들웨어 설정 */
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(express.static((path.join(__dirname, 'public'))));
 
 /* NODEJS 기본 세팅 */
 dotenv.config();
-app.set('port', process.env.DEV_PORT || 20031);
+app.set('port', 4040);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 //moment.tz.setDefault("Asia/Seoul");
@@ -29,11 +34,13 @@ app.set('views', path.join(__dirname, 'views'));
 /* NODEJS 데이터베이스 Connection */
 let databaseNam = "HspBoard";
 const databaseOption = "retryWrites=true&w=majority";
-const mongoose = require("mongoose");
-const uri = `mongodb+srv://${process.env.MONGO_ID}:${process.env.MONGO_PWD}@clustermongo.cwzee.mongodb.net/${databaseNam}?${databaseOption}`;
+
+//mongodb+srv://craham:dlHn1V8I1uzK4FvA@clustermongo.cwzee.mongodb.net/
+const id = 'craham';
+const pwd = 'dlHn1V8I1uzK4FvA';
+const uri = `mongodb+srv://${id}:${pwd}@clustermongo.cwzee.mongodb.net/${databaseNam}?${databaseOption}`;
 
 /* 라우터 설정 */
-const {boardRouter} = require('./routes/boardCRUD');
 
 /* MONGODB SERVER START */
 const server = async() =>{
